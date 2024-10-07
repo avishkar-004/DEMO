d11eb8b70745e2faaa5e6d7179c374d4b2c05acb
avishkar-004 <138965442+avishkar-004@users.noreply.github.com>
2024-10-07 18:14:58 +0530

    Add files via upload

---
+++ React App_files/main.jsx
@@ -0,0 +1,15 @@
+import { jsxDEV } from "/@id/__x00__react/jsx-dev-runtime";
+import __vite__cjsImport1_react from "/node_modules/.vite/deps/react.js?v=09403be7"; const React = __vite__cjsImport1_react.__esModule ? __vite__cjsImport1_react.default : __vite__cjsImport1_react;
+import __vite__cjsImport2_reactDom_client from "/node_modules/.vite/deps/react-dom_client.js?v=09403be7"; const ReactDOM = __vite__cjsImport2_reactDom_client.__esModule ? __vite__cjsImport2_reactDom_client.default : __vite__cjsImport2_reactDom_client;
+import App from "/src/App.jsx";
+ReactDOM.createRoot(document.getElementById("root")).render(/* @__PURE__ */ jsxDEV(React.StrictMode, { children: /* @__PURE__ */ jsxDEV(App, {}, void 0, false, {
+  fileName: "C:/Users/Avishkar/Downloads/youtube23-fiverr-ui/youtube23-fiverr-ui/src/main.jsx",
+  lineNumber: 5,
+  columnNumber: 5
+}, this) }, void 0, false, {
+  fileName: "C:/Users/Avishkar/Downloads/youtube23-fiverr-ui/youtube23-fiverr-ui/src/main.jsx",
+  lineNumber: 4,
+  columnNumber: 61
+}, this));
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJtYXBwaW5ncyI6IkFBTUk7QUFOSixPQUFPQSxXQUFXO0FBQ2xCLE9BQU9DLGNBQWM7QUFDckIsT0FBT0MsU0FBUztBQUVoQkQsU0FBU0UsV0FBV0MsU0FBU0MsZUFBZSxNQUFNLENBQUMsRUFBRUMsT0FDbkQsdUJBQUMsTUFBTSxZQUFOLEVBQ0MsaUNBQUMsU0FBRDtBQUFBO0FBQUE7QUFBQTtBQUFBLE9BQUksS0FETjtBQUFBO0FBQUE7QUFBQTtBQUFBLE9BRUEsQ0FDRiIsIm5hbWVzIjpbIlJlYWN0IiwiUmVhY3RET00iLCJBcHAiLCJjcmVhdGVSb290IiwiZG9jdW1lbnQiLCJnZXRFbGVtZW50QnlJZCIsInJlbmRlciJdLCJzb3VyY2VzIjpbIm1haW4uanN4Il0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCBSZWFjdCBmcm9tICdyZWFjdCdcbmltcG9ydCBSZWFjdERPTSBmcm9tICdyZWFjdC1kb20vY2xpZW50J1xuaW1wb3J0IEFwcCBmcm9tICcuL0FwcCdcblxuUmVhY3RET00uY3JlYXRlUm9vdChkb2N1bWVudC5nZXRFbGVtZW50QnlJZCgncm9vdCcpKS5yZW5kZXIoXG4gIDxSZWFjdC5TdHJpY3RNb2RlPlxuICAgIDxBcHAgLz5cbiAgPC9SZWFjdC5TdHJpY3RNb2RlPixcbilcbiJdLCJmaWxlIjoiQzovVXNlcnMvQXZpc2hrYXIvRG93bmxvYWRzL3lvdXR1YmUyMy1maXZlcnItdWkveW91dHViZTIzLWZpdmVyci11aS9zcmMvbWFpbi5qc3gifQ==
